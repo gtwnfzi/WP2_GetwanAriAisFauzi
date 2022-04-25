974ebe2a44b894d53b0388ee73dcbd9944d9aa4e
gtwnfzi <gtwnfzi@gmail.com>
2022-04-25 20:32:49 +0700

    test

---
+++ application/controllers/contoh_1.php
@@ -0,0 +1,14 @@
+<?php
+
+class contoh_1 extends CI_Controller {
+public function index()
+	{
+		echo "Biodata Saya<br>";
+		echo "Nama Lengkap : Getwan Ari Ais Fauzi<br>";
+		echo "Nim : 17190050<br>";
+		echo "Fakultas : Teknik dan Informasi<br>";
+		echo "Jurusan : Teknologi Informasi<br>";
+		echo "Hobi : Pragmatic Play<br>";
+		echo "Jenis Kelamin : Laki-Laki";
+	}
+}
